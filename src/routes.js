0b939514c81b2ac99137ec87327558d68158fb52
Daniel Toro <d.toro@outlook.com>
2017-04-28 13:51:16 -0300

    primer commit

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+// Containers
+import Full from './containers/Full/'
+// import Simple from './containers/Simple/'
+
+import Dashboard from './views/Dashboard/'
+import Products from './views/Products/'
+
+
+export default (
+  <Router history={hashHistory}>
+    <Route path="/" name="Home" component={Full}>
+      <IndexRoute component={Dashboard}/>
+      <Route path="dashboard" name="Dashboard" component={Dashboard}/>
+      <Route path="products" name="Products" component={Products}/>
+    </Route>
+  </Router>
+);
---
+++ src/views/Products/Products.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default Products;
